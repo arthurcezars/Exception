4bd4612aac9aee6b7c86db4697ac4dda41f0a7fd
arthurcezars <arthurcezars@gmail.com>
2021-10-11 19:34:48 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace UCLFlix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("teste\n");
+            Console.WriteLine("Matheus Teste");
+        }
+    }
+}
